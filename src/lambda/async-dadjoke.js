7fb32b00393ec5da32937a3eebcfc0ca65c2925a
Kevin Samoei <samoeikev@gmail.com>
2021-08-16 18:18:33 +0300

    update jokes api

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -4,7 +4,7 @@
 import axios from "axios"
 export async function handler(event, context) {
   try {
-    const response = await axios.get("https://icanhazdadjoke.com", { headers: { Accept: "application/json" } })
+    const response = await axios.get("https://api.chucknorris.io/jokes/random", { headers: { Accept: "application/json" } })
     const data = response.data
     console.log(data.joke)
     console.info("this is an async dad jokes serverless function")
